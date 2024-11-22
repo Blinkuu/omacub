6a3a7d8c326c1cf96bc477e7d3ea5a4455b091d1
Lukasz Gut <blinku.dev@gmail.com>
2024-11-22 22:36:18 +0100

    fix: replace absolute path with relative in app-starship.sh

--- install/terminal/app-starship.sh
+++ install/terminal/app-starship.sh
@@ -1,3 +1,3 @@
 brew install starship
 
-cp ~/.local/share/omacub/configs/starship/starship.toml /Users/lukasz/.config/starship.toml
+cp ~/.local/share/omacub/configs/starship/starship.toml ~/.config/starship.toml
