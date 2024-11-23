63e7b71a11efde4c866739fb9d40d838d4af5aea
Lukasz Gut <blinku.dev@gmail.com>
2024-11-23 22:44:25 +0100

    feat: download hack nerd font by default

--- install/desktop/fonts.sh
+++ install/desktop/fonts.sh
@@ -1,10 +1,17 @@
 mkdir -p /Library/fonts
 
 cd /tmp
+# Cascadia Mono
 wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/CascadiaMono.zip
 unzip CascadiaMono.zip -d CascadiaFont
 cp CascadiaFont/*.ttf /Library/fonts
 rm -rf CascadiaMono.zip CascadiaFont
+
+# Hack
+wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Hack.zip
+unzip Hack.zip -d HackFont
+cp HackFont/*.ttf /Library/fonts
+rm -rf Hack.zip HackFont
 
 wget -O iafonts.zip https://github.com/iaolo/iA-Fonts/archive/refs/heads/master.zip
 unzip iafonts.zip -d iaFonts
