4a78605dafb5869b420cc33f772a9230a75a8adb
Lukasz Gut <blinku.dev@gmail.com>
2024-11-23 19:11:35 +0100

    feat: update zellij.kdl config
    Fixes inability to use ctrl + <key> in vim. All operations in zellij now
    require ctrl + f to enter tmux mode before we can do normal actions.

--- install/terminal/app-zellij.sh
+++ install/terminal/app-zellij.sh
@@ -1,5 +1,6 @@
 brew install zellij
 
 mkdir -p ~/.config/zellij/themes
-[ ! -f "$HOME/.config/zellij/config.kdl" ] && cp ~/.local/share/omacub/configs/zellij.kdl ~/.config/zellij/config.kdl
+[[ -f ~/.config/zellij/config.kdl ]] && mv ~/.config/zellij/config.kdl ~/.config/zellij/config.kdl.bak
+cp ~/.local/share/omacub/configs/zellij.kdl ~/.config/zellij/config.kdl
 cp ~/.local/share/omacub/themes/tokyo-night/zellij.kdl ~/.config/zellij/themes/tokyo-night.kdl
