8d1e2e3bfb57d6ca97658cfc825f47fd56808beb
Lukasz Gut <blinku.dev@gmail.com>
2024-11-26 14:38:38 +0100

    feat: enable additional nvim plugins by default

--- install/terminal/app-neovim.sh
+++ install/terminal/app-neovim.sh
@@ -2,18 +2,19 @@ brew install neovim
 
 # Only attempt to set configuration if Neovim has never been run
 if [ ! -d "$HOME/.config/nvim" ]; then
-    # Use LazyVim
-    git clone https://github.com/LazyVim/starter ~/.config/nvim
-    # Remove the .git folder, so you can add it to your own repo later
-    rm -rf ~/.config/nvim/.git
+  # Use LazyVim
+  git clone https://github.com/LazyVim/starter ~/.config/nvim
+  # Remove the .git folder, so you can add it to your own repo later
+  rm -rf ~/.config/nvim/.git
 
-    # Make everything match the terminal transparency
-    mkdir -p ~/.config/nvim/plugin/after
-    cp ~/.local/share/omacub/configs/neovim/transparency.lua ~/.config/nvim/plugin/after/
+  # Make everything match the terminal transparency
+  mkdir -p ~/.config/nvim/plugin/after
+  cp ~/.local/share/omacub/configs/neovim/plugin/after/transparency.lua ~/.config/nvim/plugin/after/transparency.lua
 
-    # Default to Tokyo Night theme
-    cp ~/.local/share/omacub/themes/tokyo-night/neovim.lua ~/.config/nvim/lua/plugins/theme.lua
+  # Default to Tokyo Night theme
+  cp ~/.local/share/omacub/themes/tokyo-night/neovim.lua ~/.config/nvim/lua/plugins/theme.lua
 
-    # Enable default extras
-    cp ~/.local/share/omacub/configs/neovim/lazyvim.json ~/.config/nvim/lazyvim.json
+  # Enable default extras
+  cp ~/.local/share/omacub/configs/neovim/lazyvim.json ~/.config/nvim/lazyvim.json
+  cp ~/.local/share/omacub/configs/neovim/lua/plugins/*.lua ~/.config/nvim/lua/plugins/
 fi
