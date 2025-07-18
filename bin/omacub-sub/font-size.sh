1ae665b47fded7ca3fcc13fa21bed0794ad19b40
Lukasz Gut <blinku.dev@gmail.com>
2025-07-18 10:27:42 +0200

    feat: allow bigger font sizes

--- bin/omacub-sub/font-size.sh
+++ bin/omacub-sub/font-size.sh
@@ -1,8 +1,8 @@
-choice=$(gum choose {7..14} "<< Back" --height 11 --header "Choose your terminal font size")
+choice=$(gum choose {7..18} "<< Back" --height 11 --header "Choose your terminal font size")
 
 if [[ $choice =~ ^[0-9]+$ ]]; then
-	sed -i'' -e "s/^size = .*$/size = $choice/g" ~/.config/alacritty/font-size.toml
-	source $OMACUB_PATH/bin/omacub-sub/font-size.sh
+  sed -i'' -e "s/^size = .*$/size = $choice/g" ~/.config/alacritty/font-size.toml
+  source $OMACUB_PATH/bin/omacub-sub/font-size.sh
 else
-	source $OMACUB_PATH/bin/omacub-sub/font.sh
+  source $OMACUB_PATH/bin/omacub-sub/font.sh
 fi
